f7a6bf7721a39bb63a43991364e443f848a8bc02
ksinght86 <thakurks86@gmail.com>
2017-03-27 23:34:06 +0530

    compliation issue fix

--- src/com/kundan/binpacking/applet/LoadImageApplet.java
+++ src/com/kundan/binpacking/applet/LoadImageApplet.java
@@ -1,41 +1,69 @@
-package com.kundan.binpacking.core;
+package com.kundan.binpacking.applet;
 
-import java.util.ArrayList;
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
 
-import com.kundan.binpacking.utils.Utils;
+import com.kundan.binpacking.core.Bin;
+import com.kundan.binpacking.core.BinPackingAlgorithm;
+import com.kundan.binpacking.core.Item;
+import com.kundan.binpacking.core.Shelf;
 
 /**
- * Main class to run the algorithm.
- * This algorithm computes the minimum bins required to fit rectangles in unit size bin.
+ * 
  * @author my-pc
  *
  */
-public class BinPackingAlgorithm {
-	
-	public static ArrayList<Item> mItems;
-	public static ArrayList<Bin> mBins;
+public class LoadImageApplet extends Applet {
 
 	/**
-	 * main method.
-	 * @param args
+	 * 
 	 */
-	public static void init(){
-		System.out.println("Start of Algorithm");
-		// get all the items
-		// get the bin size
-		// normalize the bin to unit, and normalize the items too with same ratio.
-		// if any item exceed bin size scale it to the unit size.
-		mItems = Utils.getSampleItems();
-		System.out.println("Given Items.");
-		System.out.println(mItems);
-		
-		mBins = Utils.getNFDHBinPacking(mItems);
-		
-		// call the method which will return the packing.
-		System.out.println("End of Algorithm");
-	}
-	
-	
+	private static final long serialVersionUID = 1L;
 
-	
+	@Override
+	public void init() {
+		BinPackingAlgorithm.init();
+		super.init();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		int binPointerx = 0;
+		int binPointery = 0;
+		int count = 0;
+		for (Bin bin : BinPackingAlgorithm.mBins) {
+			int startX = binPointerx * 100;
+			int startY = binPointery * 100;
+			int shelfCurrentY = startY;
+			System.out.println(bin);
+			Random rand = new Random();
+			int colorR = rand.nextInt(256);
+			int colorG = rand.nextInt(256);
+			int colorB = rand.nextInt(256);
+			g.setColor(new Color(colorR, colorG, colorB));
+			g.drawRect(startX, startY, 100, 100);
+			for (Shelf shelf : bin.mShelves) {
+				int shelfCurrentX = startX;
+				g.setColor(new Color(colorR, colorG, rand.nextInt(256)));
+				for (Item item : shelf.mAssignedItems) {
+					g.drawRect(shelfCurrentX, shelfCurrentY,
+							(int) (item.mWidth * 100),
+							(int) (item.mHieght * 100));
+					count++;
+					g.drawString(item.index + "", shelfCurrentX, shelfCurrentY+10);
+					shelfCurrentX += (int)(100*item.mWidth);
+				}
+				shelfCurrentY += (int)(shelf.mShelfHieght*100);
+			}
+			if (binPointerx == 8) {
+				binPointerx = 0;
+				binPointery++;
+			} else {
+				binPointerx++;
+			}
+			System.out.println(count);
+		}
+	}
 }
